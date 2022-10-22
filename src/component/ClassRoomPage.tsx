a9f2eb18e93f07dbf5433c4d75bbd5e873d8908b
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-10-22 15:42:45 +0900

    ClassRoom Mobx CRUD 예제 페이지 구현

---
+++ src/component/ClassRoomPage.tsx
@@ -0,0 +1,39 @@
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { observer } from "mobx-react";
+import { user1 } from "../mockup_data/user";
+import { classRoomStore } from "../store/ClassRoomStore";
+import { ClassRoom } from "../mockup_data/classroom";
+
+const ClassRoomPage = () => {
+  const [newRoomName, setNewRoomName] = useState<String>("");
+
+  const onAddClick = () => {
+    classRoomStore.addClassRoom(newRoomName, user1.name);
+    setNewRoomName("");
+  };
+
+  const onRemoveClick = (classRoom: ClassRoom) => {
+    classRoomStore.removeClassRoom(classRoom.id);
+  };
+
+  return (
+    <div className="App">
+      <TextField
+        value={newRoomName}
+        onChange={(e) => setNewRoomName(e.target.value)}
+      ></TextField>
+      <Button onClick={onAddClick}>추가</Button>
+      {classRoomStore.rooms.map((room) => {
+        return (
+          <div key={room.id}>
+            {room.name}
+            <Button onClick={() => onRemoveClick(room)}>삭제</Button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default observer(ClassRoomPage);
