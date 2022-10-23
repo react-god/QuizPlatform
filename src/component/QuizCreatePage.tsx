16d30feed7fdc97e94064bf38979ca72781b5509
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-10-23 13:03:55 +0900

    QuizCreateStore 화면 기능 구현

---
+++ src/component/QuizCreatePage.tsx
@@ -0,0 +1,318 @@
+import {
+  Button,
+  Card,
+  Checkbox,
+  Chip,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { Add, Delete } from "@mui/icons-material";
+import { observer } from "mobx-react";
+import { useReducer, useState } from "react";
+import { QuizItem, QuizOption, QuizType } from "../mockup_data/quiz";
+import QuizCreateStore from "../store/QuizCreateStore";
+
+interface SubmitButtonProps {
+  enabled: boolean;
+  onClick: () => void;
+}
+
+const SubmitButton = (props: SubmitButtonProps) => {
+  return (
+    <Button
+      variant="contained"
+      onClick={() => props.onClick()}
+      disabled={!props.enabled}
+    >
+      <Typography variant="button">제출</Typography>
+    </Button>
+  );
+};
+
+interface NavRailItemProps {
+  number: number;
+  highlighted: boolean;
+  onClick: () => void;
+}
+
+const NavRailItem = (props: NavRailItemProps) => {
+  return (
+    <Button
+      variant="contained"
+      color={props.highlighted ? "secondary" : "inherit"}
+      onClick={() => props.onClick()}
+    >
+      {props.number}
+    </Button>
+  );
+};
+
+interface NavRailProps {
+  items: Array<QuizItem>;
+  enableSubmitButton: boolean;
+  isItemCompleted: (item: QuizItem) => boolean;
+  onItemClick: (index: number) => void;
+  onAddClick: () => void;
+  onSubmitClick: () => void;
+}
+
+const NavRail = (props: NavRailProps) => {
+  return (
+    <Paper>
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        style={{ height: "100%" }}
+      >
+        <Stack direction="column" alignItems="center">
+          {props.items.map((item, index) => {
+            return (
+              <NavRailItem
+                number={index + 1}
+                highlighted={props.isItemCompleted(item)}
+                onClick={() => props.onItemClick(index)}
+              />
+            );
+          })}
+          <IconButton onClick={() => props.onAddClick()}>
+            <Add />
+          </IconButton>
+        </Stack>
+        <SubmitButton
+          enabled={props.enableSubmitButton}
+          onClick={() => props.onSubmitClick()}
+        />
+      </Stack>
+    </Paper>
+  );
+};
+
+interface QuestionTitleProps {
+  index: number;
+  question: String;
+  showRemoveButton: boolean;
+  onQuestionChange: (question: String) => void;
+  onRemoveClick: () => void;
+}
+
+const QuestionTitle = (props: QuestionTitleProps) => {
+  return (
+    <>
+      <Typography variant="h2">
+        {props.index + 1}
+        <TextField
+          placeholder="문제 질문 입력..."
+          margin="normal"
+          size="small"
+          variant="standard"
+          value={props.question}
+          onChange={(e) => props.onQuestionChange(e.target.value)}
+        />
+        {props.showRemoveButton ? (
+          <IconButton onClick={() => props.onRemoveClick()}>
+            <Delete color="error" />
+          </IconButton>
+        ) : (
+          <></>
+        )}
+      </Typography>
+    </>
+  );
+};
+
+interface OptionChipsProps {
+  selectedType: QuizType;
+  onChipClick: (type: QuizType) => void;
+}
+
+const OptionChips = (props: OptionChipsProps) => {
+  return (
+    <div>
+      <Chip
+        label="객관식"
+        color={props.selectedType === QuizType.choice ? "primary" : "default"}
+        onClick={() => props.onChipClick(QuizType.choice)}
+      />
+      <Chip
+        label="단답식"
+        color={props.selectedType === QuizType.essay ? "primary" : "default"}
+        onClick={() => props.onChipClick(QuizType.essay)}
+      />
+    </div>
+  );
+};
+
+interface OptionBarProps {
+  title: String;
+  isAnswer: boolean;
+  showRemoveButton: boolean;
+  onTitleChange: (title: String) => void;
+  onRemoveClick: () => void;
+  onAnswerClick: (isAnswer: boolean) => void;
+  onImageClick: () => void;
+}
+
+const OptioinBar = (props: OptionBarProps) => {
+  const theme = useTheme();
+  const cardColor = props.isAnswer
+    ? theme.palette.primary.light
+    : theme.palette.background.default;
+
+  return (
+    <Card
+      elevation={2}
+      style={{
+        padding: "20px",
+        backgroundColor: cardColor,
+      }}
+    >
+      <TextField
+        variant="standard"
+        value={props.title}
+        placeholder="보기 입력..."
+        onChange={(e) => props.onTitleChange(e.target.value)}
+      ></TextField>
+      <Checkbox
+        checked={props.isAnswer}
+        onClick={() => props.onAnswerClick(!props.isAnswer)}
+      ></Checkbox>
+      {props.showRemoveButton ? (
+        <IconButton onClick={() => props.onRemoveClick()}>
+          <Delete color="error" />
+        </IconButton>
+      ) : (
+        <></>
+      )}
+    </Card>
+  );
+};
+
+interface OptionBarListProps {
+  options: Array<QuizOption>;
+  showAddButton: boolean;
+  showRemoveButton: boolean;
+  onTitleChange: (optionIndex: number, title: String) => void;
+  onAnswerClick: (optionIndex: number, isAnswer: boolean) => void;
+  onAddButtonClick: () => void;
+  onRemoveClick: (optionIndex: number) => void;
+}
+
+const OptionBarList = (props: OptionBarListProps) => {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  return (
+    <>
+      {props.options.map((option, optionIndex) => {
+        return (
+          <OptioinBar
+            key={option.uuid as string}
+            title={option.title}
+            isAnswer={option.isAnswer}
+            showRemoveButton={props.showRemoveButton}
+            onTitleChange={(title) => {
+              props.onTitleChange(optionIndex, title);
+              forceUpdate();
+            }}
+            onRemoveClick={() => props.onRemoveClick(optionIndex)}
+            onAnswerClick={(isAnswer) => {
+              props.onAnswerClick(optionIndex, isAnswer);
+              forceUpdate();
+            }}
+            onImageClick={() => {
+              // TODO
+            }}
+          />
+        );
+      })}
+      {props.showAddButton ? (
+        <OptionAddButton onClick={() => props.onAddButtonClick()} />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+interface OptionAddBarProps {
+  onClick: () => void;
+}
+
+const OptionAddButton = (props: OptionAddBarProps) => {
+  return (
+    <IconButton onClick={() => props.onClick()}>
+      <Add color="primary" />
+    </IconButton>
+  );
+};
+
+interface QuizCreatePageProps {
+  quizName: String;
+}
+
+const QuizCreatePage = (props: QuizCreatePageProps) => {
+  const [store] = useState(new QuizCreateStore(props.quizName));
+  const currentQuizItem = store.currentQuizItem;
+
+  let optionListOrEssay: JSX.Element;
+  switch (currentQuizItem.type) {
+    case QuizType.choice:
+      optionListOrEssay = (
+        <OptionBarList
+          options={store.currentQuizItem.options}
+          showAddButton={store.showQuizOptionAddButton}
+          showRemoveButton={store.showRemoveQuizOptionButton}
+          onTitleChange={(optionIndex, title) =>
+            store.updateQuizOption(optionIndex, title)
+          }
+          onAnswerClick={(optionIndex, isAnswer) =>
+            store.updateQuizOption(optionIndex, undefined, isAnswer)
+          }
+          onAddButtonClick={() => store.addQuizOption()}
+          onRemoveClick={(optionIndex) => store.removeQuizOption(optionIndex)}
+        />
+      );
+      break;
+    case QuizType.essay:
+      optionListOrEssay = (
+        <TextField
+          placeholder="답안 입력..."
+          value={currentQuizItem.essayAnswer ?? ""}
+          onChange={(e) => store.updateEssayAnswer(e.target.value)}
+        />
+      );
+      break;
+  }
+
+  return (
+    <Stack direction="row">
+      <NavRail
+        items={store.quizItems}
+        enableSubmitButton={store.enabledSubmitButton}
+        isItemCompleted={(item) => store.isItemCompleted(item)}
+        onAddClick={() => store.addQuizItem()}
+        onItemClick={(index) => (store.currentItemIndex = index)}
+        onSubmitClick={() => store.submitQuiz()}
+      />
+      <Stack direction="column" width="100%" style={{ margin: "60px" }}>
+        <QuestionTitle
+          index={store.currentItemIndex}
+          question={currentQuizItem.question}
+          showRemoveButton={store.showRemoveQuizItemButton}
+          onRemoveClick={() => store.removeCurrentQuizItem()}
+          onQuestionChange={(q) => store.updateQuizItemQuestion(q)}
+        />
+        <OptionChips
+          selectedType={currentQuizItem.type}
+          onChipClick={(type) => store.updateQuizItemType(type)}
+        />
+        {optionListOrEssay}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default observer(QuizCreatePage);
---
+++ src/store/QuizCreateStore.ts
@@ -0,0 +1,150 @@
+import { Quiz, QuizItem, QuizType } from "../mockup_data/quiz";
+import { v4 as uuidv4 } from "uuid";
+import { user1 } from "../mockup_data/user";
+import { makeAutoObservable } from "mobx";
+
+const initQuizItem: QuizItem = {
+  question: "",
+  type: QuizType.choice,
+  options: [
+    {
+      uuid: uuidv4(),
+      title: "",
+      isAnswer: false,
+    },
+    {
+      uuid: uuidv4(),
+      title: "",
+      isAnswer: false,
+    },
+  ],
+};
+
+class QuizCreateStore {
+  quiz: Quiz;
+
+  currentItemIndex: number = 0;
+
+  constructor(quizName: String) {
+    this.quiz = {
+      id: uuidv4(),
+      owner: user1,
+      name: quizName,
+      items: [initQuizItem],
+    };
+    makeAutoObservable(this);
+  }
+
+  // ------------- 퀴즈 관련 -----------------
+
+  updateQuizName(name: String) {
+    this.quiz.name = name;
+  }
+
+  get enabledSubmitButton() {
+    return this.quiz.items.every(this.isItemCompleted);
+  }
+
+  submitQuiz() {
+    // TODO(민성): ClassRoomStore에 저장하고 홈화면으로 전환하기
+  }
+
+  // ----------- 퀴즈의 항목 관련 --------------
+
+  get showQuizOptionAddButton() {
+    return this.currentQuizItem.options.length < 5;
+  }
+
+  get showRemoveQuizItemButton() {
+    return this.quiz.items.length > 1;
+  }
+
+  get showRemoveQuizOptionButton() {
+    return this.currentQuizItem.options.length > 2;
+  }
+
+  isItemCompleted(item: QuizItem) {
+    const hasQuestion = item.question.length > 0;
+    const hasAnswer =
+      item.type == QuizType.choice
+        ? item.options.some((option) => option.isAnswer)
+        : item.essayAnswer != undefined && item.essayAnswer!.length > 0;
+    const hasTitleOfAllOptions =
+      item.type == QuizType.choice
+        ? item.options.every((option) => option.title.length > 0)
+        : true;
+    return hasQuestion && hasAnswer && hasTitleOfAllOptions;
+  }
+
+  updateCurrentItemIndex(index: number) {
+    this.currentItemIndex = index;
+  }
+
+  get quizItems() {
+    return this.quiz.items;
+  }
+
+  get currentQuizItem() {
+    return this.quiz.items[this.currentItemIndex];
+  }
+
+  addQuizItem() {
+    this.quiz.items = [...this.quiz.items, initQuizItem];
+    this.currentItemIndex = this.quiz.items.length - 1;
+  }
+
+  updateQuizItemQuestion(question: String) {
+    this.currentQuizItem.question = question;
+  }
+
+  updateQuizItemType(newType: QuizType) {
+    let item = this.currentQuizItem;
+    if (item.type === newType) {
+      return;
+    }
+    item.type = newType;
+  }
+
+  updateEssayAnswer(answer: String) {
+    this.currentQuizItem.essayAnswer = answer;
+  }
+
+  removeCurrentQuizItem() {
+    this.quiz.items = this.quiz.items.filter(
+      (_, index) => index !== this.currentItemIndex
+    );
+
+    this.currentItemIndex = Math.min(
+      this.currentItemIndex,
+      this.quiz.items.length - 1
+    );
+  }
+
+  // -------- 퀴즈의 항목의 보기 관련 -------------
+
+  addQuizOption() {
+    let item = this.currentQuizItem;
+    item.options = [
+      ...item.options,
+      { uuid: uuidv4(), title: "", isAnswer: false },
+    ];
+  }
+
+  updateQuizOption(optionIndex: number, title?: String, isAnswer?: boolean) {
+    const option = this.currentQuizItem.options[optionIndex];
+    if (title !== undefined) {
+      option.title = title;
+    }
+    if (isAnswer !== undefined) {
+      option.isAnswer = isAnswer;
+    }
+  }
+
+  removeQuizOption(optionIndex: number) {
+    this.currentQuizItem.options = this.currentQuizItem.options.filter(
+      (_, index) => index !== optionIndex
+    );
+  }
+}
+
+export default QuizCreateStore;
