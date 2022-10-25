015eaed0ba801cc2a75ccdd4b29c692ce616769d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-10-25 12:21:50 +0900

    QuizCreate 화면 기능 구현 (#22)
    * QuizCreateStore 화면 기능 구현
    
    * QuizCreatePage 리렌더링 문제 수정
    
    * 디자인 개선
    
    * 현재 페이지의 글자 크기 크게 보이도록 수정
    
    * 첫번째 보기 정답으로 수정
    
    * 동등성 비교 코드 개선
    
    * 현재 페이지 글자는 크기대신 두께가 두껍게 보도록 수정
    
    * NavRail 스크롤 가능하게 수정
    
    * uuid를 QuizItem에 추가
    
    * 기본 보기 갯수 4개로 수정
    
    * 퀴즈가 단답식이면 제출 버튼 눌렀을 때 보기 없애도록 수정
    
    * Enable multiline
    
    * 정답 이유 입력 창 추가
    
    * 적응형 레이아웃 적용
    
    * 퀴즈 Image 선택 가능하게 수정
    
    * 이미지 제거 아이콘에서 텍스트로 수정
    
    * Remove useless key
    
    * 보기마다 이미지 추가할 수 있도록 수정
    
    * 스크롤 디자인 향상
    
    * NavRail Component 분리
    
    * NavRail 분리
    
    * isSelected가 true인 NavRailItem 글자 두껍게 보이도록 수정
    
    * 디렉토리 구조 수정

---
+++ src/component/NavRail.tsx
@@ -0,0 +1,95 @@
+import { Button, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+
+interface NavRailItemProps {
+  label: String;
+  color:
+    | "inherit"
+    | "secondary"
+    | "primary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning"
+    | undefined;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const NavRailItem = (props: NavRailItemProps) => {
+  return (
+    <Button
+      variant="contained"
+      style={{
+        marginTop: "4px",
+        marginBottom: "4px",
+        marginLeft: "12px",
+        marginRight: "12px",
+      }}
+      disableElevation={true}
+      color={props.color}
+      onClick={() => props.onClick()}
+    >
+      <Typography
+        variant={props.isSelected ? "h6" : "button"}
+        fontWeight={props.isSelected ? "900" : "400"}
+      >
+        <div style={{ height: "24px", alignItems: "center", display: "flex" }}>
+          {props.label}
+        </div>
+      </Typography>
+    </Button>
+  );
+};
+
+interface NavRailProps {
+  items: Array<JSX.Element>;
+  trailingItem: JSX.Element;
+  /**
+   * 콜백 함수를 전달하는 경우 뒤로 가기 버튼을 보인다.
+   */
+  onBackClick?: () => void;
+}
+
+const NavRail = (props: NavRailProps) => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      style={{
+        backgroundColor: theme.palette.grey[100],
+      }}
+    >
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        style={{ height: "100%" }}
+      >
+        <Stack
+          id="noneScrollBar"
+          direction="column"
+          alignItems="center"
+          style={{ overflowY: "scroll", paddingTop: "8px" }}
+        >
+          <IconButton
+            onClick={() => props.onBackClick?.()}
+            style={{
+              marginLeft: "12px",
+              marginRight: "12px",
+              marginTop: "8px",
+              marginBottom: "12px",
+            }}
+          >
+            <ArrowBack />
+          </IconButton>
+          {props.items.map((item) => {
+            return <>{item}</>;
+          })}
+        </Stack>
+        {props.trailingItem}
+      </Stack>
+    </Stack>
+  );
+};
+
+export { NavRail, NavRailItem };
