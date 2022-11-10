d9d5c61e7eff162502a267f50ffe265e5e3d345a
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-10 19:28:34 +0900

    Repository 삭제 및 Store로 이동

--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -1,6 +1,11 @@
 import { makeAutoObservable } from "mobx";
 import { User } from "../mockup_data/user";
-import UserRepository from "../repository/UserRepository";
+import {
+  writeJsonToLocalStorage,
+  readJsonFromLocalStorage,
+} from "../util/JsonUtil";
+
+const USER_KEY = "users";
 
 class UserStore {
   #currentUser?: User = undefined;
@@ -13,12 +18,45 @@ class UserStore {
     return this.#currentUser;
   }
 
+  /**
+   * 회원가입을 한다.
+   *
+   * 이미 동일한 ID로 가입된 회원이 있는 경우 예외를 던진다.
+   */
   signUp(id: String, password: String, name: String) {
-    this.#currentUser = UserRepository.signUp(id, password, name);
+    const users = readJsonFromLocalStorage<Array<User>>(USER_KEY) ?? [];
+
+    if (users.some((user) => user.id === id)) {
+      throw Error("이미 존재하는 ID 입니다.");
+    }
+    this.#currentUser = {
+      id: id,
+      password: password,
+      name: name,
+      invitedClassRooms: [],
+    };
+    const newUsers: User[] = [...users, this.#currentUser];
+    writeJsonToLocalStorage(USER_KEY, newUsers);
+    return this.#currentUser;
   }
 
+  /**
+   * 로그인을 시도한다.
+   *
+   * 로그인에 성공하면 currentUser에 유저 정보가 초기화된다.
+   *
+   * 존재하지 않는 ID를 입력하거나 비밀번호가 잘못된 경우 예외를 던진다.
+   */
   signIn(id: String, password: String) {
-    this.#currentUser = UserRepository.signIn(id, password);
+    const users: User[] = readJsonFromLocalStorage<User[]>(USER_KEY) ?? [];
+    const user = users.find((user) => user.id === id);
+    if (user === undefined) {
+      throw Error("존재하지 않는 ID 입니다.");
+    }
+    if (user.password !== password) {
+      throw Error("비밀번호가 잘못되었습니다.");
+    }
+    return (this.#currentUser = user);
   }
 }
 
