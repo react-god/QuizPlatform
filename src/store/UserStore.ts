525a5e6db0e4771ae5a043f4d6db5e27389daccf
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-10 19:20:59 +0900

    UserRepository 구현 (#46)

---
+++ src/store/UserStore.ts
@@ -0,0 +1,27 @@
+import { makeAutoObservable } from "mobx";
+import { User } from "../mockup_data/user";
+import UserRepository from "../repository/UserRepository";
+
+class UserStore {
+  #currentUser?: User = undefined;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  get currentUser(): User | undefined {
+    return this.#currentUser;
+  }
+
+  signUp(id: String, password: String, name: String) {
+    this.#currentUser = UserRepository.signUp(id, password, name);
+  }
+
+  signIn(id: String, password: String) {
+    this.#currentUser = UserRepository.signIn(id, password);
+  }
+}
+
+const userStore = new UserStore();
+
+export default userStore;
