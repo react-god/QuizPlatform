b6c54afc3162d85d92a22cc10fc28065b2f77e5e
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-11 10:13:07 +0900

    Fix snackbar bug

--- src/component/class_create/ClassRoomPage.tsx
+++ src/component/class_create/ClassRoomPage.tsx
@@ -174,6 +174,7 @@ const ClassRoomPage = () => {
   useEffect(() => {
     if (state !== null && "snackBarMessage" in state) {
       showSnackBar(state.snackBarMessage);
+      window.history.replaceState({}, document.title);
     }
   }, [showSnackBar, state]);
 
