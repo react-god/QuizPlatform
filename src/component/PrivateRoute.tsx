2007ae6a2783c53c1f8958dc9fc0a32e024f9138
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-13 01:05:59 +0900

    Fix lint error

--- src/component/PrivateRoute.tsx
+++ src/component/PrivateRoute.tsx
@@ -1,12 +1,7 @@
-import { ReactElement } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import userStore from "../store/UserStore";
 
-interface PrivateRouteProps {
-  children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
-}
-
-export default function PrivateRoute({}: PrivateRouteProps): React.ReactElement | null {
+export default function PrivateRoute(): React.ReactElement | null {
   const isAuthenticated = userStore.currentUser !== undefined;
 
   return !isAuthenticated ? <Navigate to="/login" /> : <Outlet />;
